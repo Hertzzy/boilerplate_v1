bc2d21c998bd74059ce843725276692923006c2b
Hertzyy <helter.xavier@hotmail.com>
2024-08-23 15:46:53 -0300

    Adição da datable

--- src/components/HeaderView.tsx
+++ src/components/HeaderView.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { FaMoon, FaSun } from 'react-icons/fa';
+// import { FaMoon, FaSun } from 'react-icons/fa';
 import { useTheme } from '../context/ThemeContext';
 
 interface HeaderViewProps {
@@ -16,24 +16,24 @@ const PageTitle = styled.h1<{ $isDarkMode: boolean }>`
   color: ${({ $isDarkMode }) => ($isDarkMode ? '#fff' : '#536d82')}; // Cor do título baseado no tema
 `;
 
-const ThemeToggleButton = styled.button<{ $isDarkMode: boolean }>`
-  background: none;
-  border: none;
-  color: ${({ $isDarkMode }) => ($isDarkMode ? '#fff' : '#536d82')}; // Cor do ícone baseado no tema
-  font-size: 24px;
-  cursor: pointer;
-  transition: left 0.3s;
-  margin-right: 15px;
-`;
+// const ThemeToggleButton = styled.button<{ $isDarkMode: boolean }>`
+//   background: none;
+//   border: none;
+//   color: ${({ $isDarkMode }) => ($isDarkMode ? '#fff' : '#536d82')}; // Cor do ícone baseado no tema
+//   font-size: 24px;
+//   cursor: pointer;
+//   transition: left 0.3s;
+//   margin-right: 15px;
+// `;
 
 const HeaderView: React.FC<HeaderViewProps> = ({ pageTitle }) => {
-  const { isDarkMode, toggleTheme } = useTheme();
+  const { isDarkMode } = useTheme();
   return (
     <HeaderContainer $isDarkMode={isDarkMode}>
       <PageTitle $isDarkMode={isDarkMode}>{pageTitle}</PageTitle>
-      <ThemeToggleButton $isDarkMode={isDarkMode} onClick={toggleTheme}>
+      {/* <ThemeToggleButton $isDarkMode={isDarkMode} onClick={toggleTheme}>
         {isDarkMode ? <FaSun /> : <FaMoon />}
-      </ThemeToggleButton>
+      </ThemeToggleButton> */}
     </HeaderContainer>
   );
 };
--- src/views/search/UserSearch.tsx
+++ src/views/search/UserSearch.tsx
@@ -1,15 +1,110 @@
 import Button from '@mui/material/Button';
 import HeaderView from '../../components/HeaderView';
-import DataTable from '../../components/Table/DataTable';
+import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
+import styled from 'styled-components';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import Tooltip from '@mui/material/Tooltip';
+import api from '../../service/api'; // Importa a instância do Axios
+import { useEffect, useState } from 'react';
+
+// Defina as funções para manipulação de eventos
+const handleShow = (id: number) => {
+  console.log(`Mostrar detalhes do item com id: ${id}`);
+};
+
+const handleDelete = (id: number) => {
+  console.log(`Deletar item com id: ${id}`);
+};
+
+// Defina as colunas da tabela
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', flex: 1 },
+  { field: 'name', headerName: 'Nome', flex: 1 },
+  {
+    field: 'status',
+    headerName: 'Status',
+    flex: 1,
+    renderCell: (params: GridRenderCellParams) => (
+      <span>{params.value === 1 ? 'Ativo' : params.value === 2 ? 'Inativo' : 'Desconhecido'}</span>
+    )
+  },
+  { field: 'role_name', headerName: 'Permissão', flex: 1 },
+  {
+    field: 'ações',
+    headerName: 'Ações',
+    flex: 1,
+    renderCell: (params: GridRenderCellParams) => (
+      <>
+        <Tooltip title="Mostrar">
+          <IconButton aria-label="show" onClick={() => handleShow(params.row.id)}>
+            <VisibilityIcon />
+          </IconButton>
+        </Tooltip>
+
+        <Tooltip title="Deletar">
+          <IconButton aria-label="delete" onClick={() => handleDelete(params.row.id)}>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      </>
+    )
+  }
+];
+
+const TableContainer = styled.div`
+  width: 99%;
+  display: flex;
+  overflow: auto;
+  margin: 25px 0 0 0;
+`;
 
 const UserSearch = () => {
+  const [rows, setRows] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get('/users');
+        const data = response.data;
+
+        // Transforme os dados para o formato que a tabela espera
+        const formattedData = data.map((user: any) => ({
+          id: user.id,
+          name: user.name,
+          status: user.status,
+          role_name: user.roles[0]?.description || 'Desconhecido'
+        }));
+
+        setRows(formattedData);
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <HeaderView pageTitle="Pesquisar Usuário" />
       <form>
         <Button variant="contained">Contained</Button>
       </form>
-      <DataTable />
+      <TableContainer>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 10 }
+            }
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
+      </TableContainer>
     </>
   );
 };
